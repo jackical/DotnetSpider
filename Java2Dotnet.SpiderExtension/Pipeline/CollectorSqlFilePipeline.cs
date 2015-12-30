92ff1fd193b67264a0d0bd749dcdbb518f7c900e
zlzforever@163.com <zlzforever@163.com>
2015-12-30 09:25:56 +0800

    Add pipile to save mysql sql file

---
+++ Java2Dotnet.SpiderExtension/Pipeline/CollectorSqlFilePipeline.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Text;
+using Java2Dotnet.Spider.Core;
+
+namespace Java2Dotnet.Spider.Extension.Pipeline
+{
+	public class CollectorMySqlFilePipeline : IPageModelPipeline
+	{
+		private static string _folderPath;
+
+		public CollectorMySqlFilePipeline()
+		{
+			_folderPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "DotnetSpiderData");
+
+			if (!Directory.Exists(_folderPath))
+			{
+				Directory.CreateDirectory(_folderPath);
+			}
+		}
+
+		[MethodImpl(MethodImplOptions.Synchronized)]
+		public void Process(Dictionary<Type, List<dynamic>> data, ISpider spider)
+		{
+			foreach (var pair in data)
+			{
+				Type type = pair.Key;
+				bool isGeneric = typeof(IEnumerable).IsAssignableFrom(type);
+				Type actualType = isGeneric ? type.GenericTypeArguments[0] : type;
+				foreach (var r in pair.Value)
+				{
+					string filePath = Path.Combine(_folderPath, actualType.Name + ".sql");
+					
+					var properties = actualType.GetProperties();
+
+					StringBuilder builder = new StringBuilder();
+					foreach (var propertyInfo in properties)
+					{
+						builder.Append("'").Append(propertyInfo.GetValue(r).ToString()).Append("'").Append(",");
+					}
+					builder.Remove(builder.Length - 1, 1);
+					builder.Append(Environment.NewLine);
+
+					File.AppendAllText(filePath, builder.ToString(), Encoding.UTF8);
+				}
+			}
+		}
+	}
+}
